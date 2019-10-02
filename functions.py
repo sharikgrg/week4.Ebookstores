6da347bc13c2433ece9ff421bda3c7259bd8a953
Sharik Gurung <SGurung@spartaglobal.com>
2019-10-02 19:51:02 +0100

    defined function to add by name

--- functions.py
+++ functions.py
@@ -1,4 +1,15 @@
 from connecting_SQLdb import *
 
-def retrieve_all_books(query):
-    return cursor.execute("SELECT * FROM books")
+# def retrieve_all_books():
+#     table = (cursor.execute("SELECT * FROM books")).fetchall()
+#     for list in table:
+#         print (list)
+
+def retrieve_all_books():
+    table = (cursor.execute("SELECT * FROM books")).fetchall()
+    for list in table:
+        print (list)
+
+def retrieve_book(name):
+    data = (cursor.execute(f"SELECT Author, Title, Dates FROM books WHERE Author = {name}")).fetchall()
+    print(data)
--- run.commands.py
+++ run.commands.py
@@ -1,5 +1,7 @@
 from functions import *
 from connecting_SQLdb import *
 
+retrieve_all_books()
 
 
+retrieve_book("'KALOK'")
